d3330d114500c0f58216f9c3834a588b03c421bc
Sebastian <sburgos1981@gmail.com>
2023-12-09 18:59:55 -0300

    Empezamos a unificar todo

--- fin.py
+++ fin.py
@@ -1,64 +1,69 @@
-stock_mouse = {
-    "M001": {
-        "tipo": "mouse",
-        "marca": "logitech",
-        "modelo": "m 100 usb",
-        "precio": 2000,
-        "stock_actual": 10,
-    },
-    "M002": {
-        "tipo": "mouse",
-        "marca": "acer predator",
-        "modelo": "cestus 500 gamer",
-        "precio": 3000,
-        "stock_actual": 15,
-    },
-}
-stock_teclado = {
-    "TC01": {
-        "tipo": "teclado",
-        "marca": "maxell",
-        "modelo": "KB-90",
-        "precio": 9000,
-        "stock_actual": 5,
-    },
-    "TC02": {
-        "tipo": "teclado",
-        "marca": "lenovo",
-        "modelo": "G 470",
-        "precio": 5000,
-        "stock_actual": 4,
-    },
-}
-stock_monitor = {
-    "MN01": {
-        "tipo": "monitor",
-        "marca": "noblex",
-        "modelo": "LFT2534",
-        "precio": 45000,
-        "stock_actual": 3,
-    },
-    "MN02": {
-        "tipo": "monitor",
-        "marca": "samsung",
-        "modelo": "sm5785",
-        "precio": 75000,
-        "stock_actual": 5,
-    },
-}
-stock = {
-    "mouse": stock_mouse,
-    "teclado": stock_teclado,
-    "monitor": stock_monitor,
-}
+class Producto:
+    def _init_(self, tipo, marca, modelo, precio, stock_actual):
+        self.tipo = tipo
+        self.marca = marca
+        self.modelo = modelo
+        self.precio = precio
+        self.stock_actual = stock_actual
+
+
+class Stock:
+    def _init_(self):
+        self.stock = {}
+
+    def agregar_producto(self, categoria, codigo, producto):
+        if categoria not in self.stock:
+            self.stock[categoria] = {}
+        self.stock[categoria][codigo] = producto
+
+    def eliminar_producto(self, codigo):
+        for categoria, productos in self.stock.items():
+            if codigo in productos:
+                del productos[codigo]
+                print(f"{codigo} ha sido eliminado.")
+                return
+        print("CÓDIGO NO VÁLIDO.")
+
+    def mostrar_stock_completo(self):
+        for categoria, productos in self.stock.items():
+            print(categoria.upper())
+            for codigo, producto in productos.items():
+                print(len(categoria) * " ", codigo)
+                for atributo, valor in producto._dict_.items():
+                    print(len(categoria + codigo) * " ", atributo, ":", valor)
+
+    def mostrar_produc_por_categoria(self, categoria):
+        if categoria not in self.stock:
+            print("CATEGORÍA INEXISTENTE.")
+            return
+
+        print("INFORME DE CATEGORÍA", categoria)
+        for codigo, producto in self.stock[categoria].items():
+            print(codigo)
+            for atributo, valor in producto._dict_.items():
+                print(len(codigo) * " ", atributo, ":", valor)
+
+    def mostrar_por_codigo(self, codigo):
+        encontrado = False
+        for categoria in self.stock:
+            if codigo in self.stock[categoria]:
+                encontrado = True
+                print("Las características del producto", codigo, "son:")
+                for atributo, valor in self.stock[categoria][codigo]._dict_.items():
+                    print(atributo, ":", valor)
+                break
+
+        if not encontrado:
+            print("CÓDIGO INEXISTENTE.")
+
+
+stock = Stock()  # Instancia de la clase Stock
 
 usuario = "admin"
 password = "1234"
 
-print("Bienvenido a nuestra tienda")
-print()
 
-def login():  
+def login():
     for i in range(3):
         user = input("Ingrese el nombre de usuario: ")
         if usuario == user.lower():
@@ -66,15 +71,14 @@ def login():
                 contrasenia = input("Ingrese la contraseña: ")
                 if password == contrasenia:
                     print("Bienvenido a tecno house")
-                    return True  
+                    return True
                 else:
                     print("Error al ingresar")
             break
         else:
             print("Error de usuario")
-    print(
-        "Usted ha superado el limite de intentos de login. "
-    )  
+    print("Usted ha superado el límite de intentos de login.")
+
 
 def menu_principal():
     while True:
@@ -82,32 +86,24 @@ def menu_principal():
         print("Seleccione una opción:")
         print("1. GESTION DE PRODUCTOS")
         print("2. INFORMES")
-        print("3. CERRAR SESION")
+        print("3. CERRAR SESIÓN")
         opcion = input()
-        while (
-            opcion != "1" and opcion != "2" and opcion != "3"
-        ):  
+        while opcion not in ["1", "2", "3"]:
             opcion = input("Opción no válida. Reingrese:")
-        match opcion:
-            case "1":
-                print("Elegiste gestion de productos")
-                gestion_productos()
-            case "2":
-                print("Elegiste informes")
-                menu_informes()
-            case "3":
-                print("Elegiste cerrar sesión.\nHasta pronto")
-                break
+        if opcion == "1":
+            print("Elegiste gestion de productos")
+            gestion_productos()
+        elif opcion == "2":
+            print("Elegiste informes")
+            menu_informes()
+        elif opcion == "3":
+            print("Elegiste cerrar sesión.\nHasta pronto")
+            break
 
 
 def gestion_productos():
     print("MENU GESTION DE PRODUCTOS")
-    opciones = [
-        "1",
-        "2",
-        "3",
-        "4",
-    ]  
+    opciones = ["1", "2", "3", "4"]
     opcion = None
     while opcion not in opciones:
         print("Que desea realizar? ")
@@ -115,12 +111,10 @@ def gestion_productos():
         print("2. MODIFICAR PRODUCTO EXISTENTE")
         print("3. ELIMINAR UN PRODUCTO")
         print("4. VOLVER AL MENU PRINCIPAL")
-        opcion = (
-            input()
-        )  
+        opcion = input()
         if opcion not in opciones:
             print("OPCION NO VALIDA.")
-        
+
     if opcion == "1":
         agregar_producto()
     elif opcion == "2":
@@ -136,42 +130,44 @@ def menu_informes():
     print("¿Que desea realizar?")
     print("1.Mostrar el stock completo")
     print("2.Mostrar productos por categoria")
-    print("3.Mostrtar las caracteristicas de un producto")
+    print("3.Mostrar las caracteristicas de un producto")
     opcion = input()
     if opcion == "1":
-        mostrar_stock_completo()
+        stock.mostrar_stock_completo()
     elif opcion == "2":
-        mostrar_produc_por_categoria()
+        categoria = input("Ingrese la categoría: ")
+        stock.mostrar_produc_por_categoria(categoria)
     elif opcion == "3":
-        mostrar_por_codigo()
+        codigo = input("Ingrese el código del producto: ")
+        stock.mostrar_por_codigo(codigo)
     else:
         print("Opcion no valida. Reintente")
 
 
 def modificar_prod_existente():
     categoria = None
-    
+
     print("Las categorías existentes son:")
-    for i in stock:
+    for i in stock.stock:
         print(i)
 
-    while categoria not in stock:
+    while categoria not in stock.stock:
         categoria = input("Ingrese la categoría a modificar: ").lower()
-        if categoria not in stock:
+        if categoria not in stock.stock:
             print("Categoría inexistente, reintente.")
 
     print(f"Los códigos de productos en la categoría {categoria} son:")
-    for j in stock[categoria]:
+    for j in stock.stock[categoria]:
         print(j)
 
     codigo = input("Ingrese el código del producto a modificar: ").upper()
 
-    while codigo not in stock[categoria]:
+    while codigo not in stock.stock[categoria]:
         print("Código inexistente. Por favor, ingrese un código válido.")
         codigo = input("Ingrese el código del producto a modificar: ").upper()
 
     print("Atributos actuales del producto:")
-    for atributo, valor in stock[categoria][codigo].items():
+    for atributo, valor in stock.stock[categoria][codigo]._dict_.items():
         print(f"{atributo}: {valor}")
 
     while True:
@@ -182,7 +178,7 @@ def modificar_prod_existente():
         if atributo == "salir":
             break
 
-        if atributo not in stock[categoria][codigo]:
+        if atributo not in stock.stock[categoria][codigo]._dict_:
             print("Atributo no válido. Intente nuevamente.")
             continue
 
@@ -191,10 +187,10 @@ def modificar_prod_existente():
         else:
             valor = float(input(f"Ingrese el nuevo valor de {atributo}: "))
 
-        stock[categoria][codigo][atributo] = valor
+        setattr(stock.stock[categoria][codigo], atributo, valor)
 
     print("Atributos actualizados del producto:")
-    for atributo, valor in stock[categoria][codigo].items():
+    for atributo, valor in stock.stock[categoria][codigo]._dict_.items():
         print(f"{atributo}: {valor}")
 
 
@@ -210,28 +206,24 @@ def agregar_producto():
 
         if opcion == "1":
             print("Categorías disponibles:")
-            for categoria in stock:
+            for categoria in stock.stock:
                 print(categoria)
             categoria = input("Ingrese la categoría del producto: ")
 
-            while categoria not in stock:
+            while categoria not in stock.stock:
                 print("Categoría no válida. Vuelva a intentar.")
                 categoria = input("Ingrese la categoría del producto: ")
         else:
             categoria = input("Ingrese la nueva categoría: ")
-            stock[categoria] = {}
+            stock.stock[categoria] = {}
 
         codigo = input("Ingrese el código del producto: ")
         marca = input("Ingrese la marca: ")
         modelo = input("Ingrese el modelo: ")
         precio = float(input("Ingrese el precio: "))
         stock_actual = int(input("Ingrese la cantidad: "))
-        stock[categoria][codigo] = {
-            "precio": precio,
-            "marca": marca,
-            "modelo": modelo,
-            "stock_actual": stock_actual,
-        }
+        producto = Producto(tipo=categoria, marca=marca, modelo=modelo, precio=precio, stock_actual=stock_actual)
+        stock.agregar_producto(categoria, codigo, producto)
 
         continuar = input("¿Desea continuar agregando productos? (s/n)").lower()
 
@@ -247,24 +239,7 @@ def agregar_producto():
 def eliminar_producto():
     while True:
         codigo = input("Ingrese el codigo del producto a eliminar: ").upper()
-        encontrado = False
-        
-        for categoria, productos in stock.items():
-            
-            if codigo in productos:
-                del productos[codigo]
-                encontrado = True
-            
-                print(f"{codigo} ha sido eliminado.")
-                break
-            
-        if not encontrado:
-            print("CÓDIGO NO VÁLIDO.")
-            print("Los códigos existentes son:")
-            for categoria, productos in stock.items():
-                print(f"{categoria}:")
-                for clave in productos.keys():
-                    print(clave)
+        stock.eliminar_producto(codigo)
 
         respuesta = input("¿Desea eliminar otro producto? (s/n)").lower()
         while respuesta != "s" and respuesta != "n":
@@ -275,61 +250,6 @@ def eliminar_producto():
         if respuesta == "n":
             break
 
-    
-
-def mostrar_stock_completo():
-    for categoria, productos in stock.items():
-        
-        print(categoria.upper())
-        
-        for codigo, atributos in productos.items():
-            print(len(categoria) * " ", codigo)
-            
-            for descripcion, valor in atributos.items():
-                print(len(categoria + codigo) * " ", descripcion, ":", valor)
-
-def mostrar_produc_por_categoria():
-    continuar = "s"
-    while continuar == "s":
-        categoria = input("¿De qué categoría desea obtener el informe?: ")
-        while categoria not in stock:
-            print("CATEGORÍA INEXISTENTE. Las categorías existentes son:")
-            for i in stock:
-                print(i)
-            categoria = input("Ingrese una categoría válida:\n")
-        print("INFORME DE CATEGORÍA", categoria)
-        for codigo, atributos in stock[categoria].items():
-            print(codigo)
-            for descripcion, valor in atributos.items():
-                print(len(codigo) * " ", descripcion, ":", valor)
-        continuar = input("¿Desea obtener el informe de otra categoría? (s/n)").lower()
-        while continuar != "s" and continuar != "n":
-            continuar = input("DATO NO VÁLIDO. ¿Desea obtener el informe de otra categoría? (s/n)").lower()
-
-def mostrar_por_codigo():
-    print("INFORME DE PRODUCTO")
-    continuar = "s"
-    while continuar == "s":
-        codigo = input("Ingrese el código del producto: ").upper()
-        encontrado = False
-        while not encontrado:
-            for categoria in stock:
-                if codigo in stock[categoria]:
-                    encontrado = True
-                    break
-            if not encontrado:
-                codigo = input("CÓDIGO INEXISTENTE. Reingrese el código: ").upper()
-        print("Las características del producto", codigo,"son: ")
-        for categoria in stock:
-            if codigo in stock[categoria]:
-                for atributo, valor in stock[categoria][codigo].items():
-                    print(atributo, ":", valor)
-                break
-        continuar = input("¿Desea ver otro producto? (s/n): ").lower()
-        while continuar != "s" and continuar != "n":
-            continuar = input("Dato invalido. ¿Desea ver otro producto? (s/n): ").lower()
-
 
 if login():
-
- menu_principal()
+    menu_principal()
